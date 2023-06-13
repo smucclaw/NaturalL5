15d4ca8de4d93c98958d43fdb51f325e369574ca
Jules <julia.poo.poo.poo@gmail.com>
2023-06-13 16:32:52 +0800

    Added testing infra, not tested

---
+++ Elaborate-L5/parser/test/index.test.ts
@@ -0,0 +1,8 @@
+import {describe, expect, test} from '@jest/globals';
+import {f} from "../src/index"
+
+describe("Testing f", () => {
+    test("Output of f has to be 1", () => {
+        expect(f()).toBe(1)
+    });
+});
