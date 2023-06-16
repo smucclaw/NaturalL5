1840cc13ce2eac1aa16ed2dc5b3ec385b47e0466
lczm <zemingchua@gmail.com>
2023-06-16 14:54:15 +0800

    Test variable declarations

--- web-runtime-prototype/test/Parser.test.ts
+++ web-runtime-prototype/test/Parser.test.ts
@@ -1,1 +1,21 @@
 import { describe, expect, test } from "@jest/globals";
+import { lex } from "../src/Lexer";
+import { parse } from "../src/Parser";
+import * as Ast from "../src/AstNode";
+
+describe("Parser", () => {
+  test("Variable declarations", () => {
+    const test_string = `
+      var a = 10;
+      var b = 20;
+    `;
+    const ast = parse(lex(test_string));
+
+    const test_stmts = Array<Ast.Stmt>();
+    test_stmts.push(new Ast.ConstDecl("a", new Ast.Literal(10)));
+    test_stmts.push(new Ast.ConstDecl("b", new Ast.Literal(20)));
+    const test_block = new Ast.Block(test_stmts);
+
+    expect(ast.toString()).toBe(test_block.toString());
+  });
+});
