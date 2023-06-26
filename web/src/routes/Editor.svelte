14757ace519e5719a8083ab6251e5d5e1f954af0
lczm <zemingchua@gmail.com>
2023-06-26 14:36:28 +0800

    Move editor into web

---
+++ web/src/routes/Editor.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+	import loader from '@monaco-editor/loader';
+	import { onDestroy, onMount } from 'svelte';
+	import type * as Monaco from 'monaco-editor/esm/vs/editor/editor.api';
+	import type { Writable } from 'svelte/store';
+
+	// Store configs
+	export let editor_value: Writable<string>;
+
+	// let editor: Monaco.editor.IStandaloneCodeEditor;
+	let monaco: typeof Monaco;
+	let editorContainer: HTMLElement;
+
+	onMount(async () => {
+		const monacoEditor = await import('monaco-editor');
+		loader.config({ monaco: monacoEditor.default });
+
+		monaco = await loader.init();
+
+		// Create monaco instance
+		const editor = monaco.editor.create(editorContainer, {
+			// Disbale minimap, more options can be built on here
+			minimap: {
+				enabled: false
+			},
+			automaticLayout: true
+		});
+		const model = monaco.editor.createModel(
+			$editor_value,
+			undefined
+			// Syntax highlighting
+			// monaco.Uri.file('sample.js')
+		);
+		editor.setModel(model);
+
+		// Monaco on change event, this can only be called post-mount
+		// Update the store with the editor value
+		editor.getModel()?.onDidChangeContent((_) => {
+			editor_value.set(editor.getValue());
+		});
+	});
+
+	onDestroy(() => {
+		monaco?.editor.getModels().forEach((model) => model.dispose());
+	});
+</script>
+
+<div class="editor" bind:this={editorContainer} />
+
+<style>
+	.editor {
+		width: 100%;
+		height: 100%;
+		/* border: 1px; */
+		/* border-color: black; */
+		/* border-style: solid; */
+	}
+</style>
