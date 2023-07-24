8c14151ae9c50d319ae0deb1eafd15686ece832e
Jules <julia.poo.poo.poo@gmail.com>
2023-07-25 02:26:51 +0800

    WebDSL/Fix: Buncha bugs including the exponentially many calls to the evaluator

--- web/src/routes/Form.svelte
+++ web/src/routes/Form.svelte
@@ -15,11 +15,12 @@
 	import Question from './Question.svelte';
 	import Justification from './Justification.svelte';
 	import { format_trace, type TraceFormatted } from '../../../web-runtime-prototype/src/TraceAst';
+	import type { Maybe } from '../../../web-runtime-prototype/src/utils';
 
 	export let input: Writable<string>;
 	export let current_question_answers: Writable<Map<string, Ast.PrimitiveType>>;
 	export let current_question_type: Writable<Map<string, string>>;
-	export let final: Writable<number>;
+	export let final: Writable<Maybe<number>>;
 	export let logger: Writable<string>;
 	export let question_validity: Writable<Map<string, boolean>>;
 	export let justification_trace: Writable<TraceFormatted | undefined>;
@@ -39,7 +40,7 @@
 		}
 		if (x instanceof EventWaiting) {
 			// console.log('WAITING  :', x.userinput.toString());
-			final.set(0);
+			final.set(undefined);
 		}
 	};
 
@@ -47,12 +48,13 @@
 	const input_callback = (ctx: EvaluatorContext, userinput: Ast.UserInputLiteral) => {
 		// Get the question as a string
 		const question = userinput.callback_identifier;
+		current_question_type.update((map) => map.set(question, userinput.type));
 		// When the evaluator tries to evaluate the UserInput,
 		// it'll call the registered input_callback (this)
 		ctx.register_input_callback(question, (evt) => {
 			// If its a request, we want to "ask" the user the question
 			if (evt instanceof EventRequest) {
-				handle_request(question, userinput.type, evt);
+				handle_request(question, evt);
 			}
 			// We make it so that they can answer the question
 			if (evt instanceof EventValidate) {
@@ -65,19 +67,19 @@
 		});
 	};
 
-	const handle_request = (question: string, type: string, evt: EventRequest) => {
+	const prevmap = new Map<string, Ast.PrimitiveType>();
+	const handle_request = (question: string, evt: EventRequest) => {
 		current_question_answers.update((map) => map.set(question, undefined));
-		current_question_type.update((map) => map.set(question, type));
 
-		let prev: Ast.PrimitiveType = undefined;
 		current_question_answers.subscribe((d) => {
 			// Get value back from the form
-			if (d.get(question) != prev && d.get(question) != undefined) {
-				prev = d.get(question);
+			const c = d.get(question);
+			if (c != prevmap.get(question) && c != undefined) {
+				prevmap.set(question, c);
 				// Try to continue evaluation, if it errors out then log it to the console
 				// This is for evaluation error messages
 				try {
-					evt.cont(prev);
+					evt.cont(c);
 					logger.set('Ran fine!');
 				} catch (error) {
 					logger.set(error as string);
@@ -112,10 +114,10 @@
 	onMount(() => {
 		// Run once when mounted
 		evaluate_program($input);
+		current_question_answers.set(new Map());
+		current_question_type.set(new Map());
 		// Run every time the input gets changed
 		input.subscribe((user_input) => {
-			current_question_answers.set(new Map());
-			current_question_type.set(new Map());
 			evaluate_program(user_input);
 		});
 	});
@@ -126,7 +128,7 @@
 		<Question question={q} type={$current_question_type.get(q)} />
 	{/each}
 
-	{#if $final == 0}
+	{#if $final == undefined}
 		<div id="result">
 			<p>Claimable : Not enough questions answered to compute a claimable sum</p>
 		</div>
@@ -139,7 +141,7 @@
 	{#if $justification_trace != undefined}
 		<div>
 			<p>Justification Trace:</p>
-			<Justification trace={$justification_trace} template={$justification_trace.template}/>
+			<Justification trace={$justification_trace} />
 		</div>
 	{/if}
 </div>
